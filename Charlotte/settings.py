84fe735b763ec531da723f2224a9ff555ad806a6
valeera li <valeera li>
2017-05-06 08:10:36 +0800

    add restfull api

--- Charlotte/settings.py
+++ Charlotte/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'spiders',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -128,3 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST_FRAMEWORK
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
